fdc9e8108bade4abb52b324dfc0b7f2c351e7e65
Gusarich <danielsedovzzz@gmail.com>
2023-11-03 00:17:13 +0300

    update email

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,53 +1,56 @@
-import { logo, telegram, email, html, css, ton, python, ts, telegramBlue } from "../assets"
-
-const LOGO = {
-	img: logo,
-	title: "",
-	href: "",
-	id: "Logo"
-}
-
-const NavBarButtons = [
-	{
-		logo: telegram,
-		title: "Telegram",
-		bg: "bg-BLUE",
-		color: "ColorWhite",
-		href: "https://t.me/coinvent_dev",
-	}, 
-	{
-		logo: email,
-		title: "Email",
-		bg: "bg-WHITE",
-		color: "ColorBlue",
-		href: "mailto:business@coinvent.dev"
-	},
-]
-
-const FooterButtons = [
-	{
-		logo: telegramBlue,
-		title: "Telegram",
-		bg: "bg-WHITE",
-		color: "ColorBlue",
-		href: "https://t.me/coinvent_dev",
-	}, 
-	{
-		logo: email,
-		title: "Email",
-		bg: "bg-WHITE",
-		color: "ColorBlue",
-		href: "mailto:business@coinvent.dev"
-	},
-]
-
-const langs = [
+import {
+    logo,
+    telegram,
+    email,
     html,
     css,
     ton,
     python,
-    ts,    
-]
+    ts,
+    telegramBlue,
+} from '../assets';
 
-export { LOGO, NavBarButtons, FooterButtons, langs, };
-  
+const LOGO = {
+    img: logo,
+    title: '',
+    href: '',
+    id: 'Logo',
+};
+
+const NavBarButtons = [
+    {
+        logo: telegram,
+        title: 'Telegram',
+        bg: 'bg-BLUE',
+        color: 'ColorWhite',
+        href: 'https://t.me/coinvent_dev',
+    },
+    {
+        logo: email,
+        title: 'Email',
+        bg: 'bg-WHITE',
+        color: 'ColorBlue',
+        href: 'mailto:contact@coinvent.dev',
+    },
+];
+
+const FooterButtons = [
+    {
+        logo: telegramBlue,
+        title: 'Telegram',
+        bg: 'bg-WHITE',
+        color: 'ColorBlue',
+        href: 'https://t.me/coinvent_dev',
+    },
+    {
+        logo: email,
+        title: 'Email',
+        bg: 'bg-WHITE',
+        color: 'ColorBlue',
+        href: 'mailto:contact@coinvent.dev',
+    },
+];
+
+const langs = [html, css, ton, python, ts];
+
+export { LOGO, NavBarButtons, FooterButtons, langs };
